cd25f39f102a94ea4e7c2a976775ac368fa62580
BlitzCityDIY <blitzcitydiy@gmail.com>
2018-12-11 18:55:16 -0500

    Updated license

--- Chapter 11/Practical Tinker Board_Chap. 11_Code/roboTinker.py
+++ Chapter 11/Practical Tinker Board_Chap. 11_Code/roboTinker.py
@@ -1,4 +1,16 @@
-#  Requires the Adafruit Motor HAT Python Library
+'''
+Python library for interfacing with the Adafruit Motor HAT for Raspberry Pi to control DC motors with speed control and Stepper motors with single, double, interleave and microstepping.
+
+Designed specifically to work with the Adafruit Motor Hat
+
+----> https://www.adafruit.com/product/2348
+
+Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
+
+Written by Limor Fried for Adafruit Industries. MIT license, all text above must be included in any redistribution
+'''
+
+#  Requires the Adafruit Motor HAT Python Library: https://github.com/adafruit/Adafruit-Motor-HAT-Python-Library
 
 import time
 import Robot
