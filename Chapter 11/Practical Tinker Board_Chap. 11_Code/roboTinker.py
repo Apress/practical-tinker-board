79ff93e58cae5423c34909b7af491898b3142a01
BlitzCityDIY <blitzcitydiy@gmail.com>
2018-12-11 19:14:14 -0500

    Updated license

--- Chapter 11/Practical Tinker Board_Chap. 11_Code/roboTinker.py
+++ Chapter 11/Practical Tinker Board_Chap. 11_Code/roboTinker.py
@@ -1,13 +1,19 @@
 '''
-Python library for interfacing with the Adafruit Motor HAT for Raspberry Pi to control DC motors with speed control and Stepper motors with single, double, interleave and microstepping.
+The MIT License
 
-Designed specifically to work with the Adafruit Motor Hat
+Copyright <2018> <Liz Clark>
 
-----> https://www.adafruit.com/product/2348
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
 
-Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-Written by Limor Fried for Adafruit Industries. MIT license, all text above must be included in any redistribution
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
 #  Requires the Adafruit Motor HAT Python Library: https://github.com/adafruit/Adafruit-Motor-HAT-Python-Library
